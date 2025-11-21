c6c5e5e8f0366034e535bfb9a996bd2f8389c64b
Jack Toumey <jack@toumey.io>
2025-11-21 14:02:41 -0700

    using summary form of chat memory

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import (
+  ConversationBufferMemory,
+  FileChatMessageHistory,
+  ConversationSummaryMemory,
+)
 
 # from dotenv import load_dotenv
 from langchain.prompts import (
   MessagesPlaceholder,
   HumanMessagePromptTemplate,
-  ChatMessagePromptTemplate,
+  # ChatMessagePromptTemplate,
   ChatPromptTemplate,
 )
 
 # load_dotenv()
 
-chat = ChatOpenAI()
-memory = ConversationBufferMemory(
-  chat_memory=FileChatMessageHistory("messages.json"),
+chat = ChatOpenAI(
+  verbose=True,
+)
+memory = ConversationSummaryMemory(
   memory_key="messages",
   return_messages=True,
+  llm=chat,
 )
+# memory = ConversationBufferMemory(
+#   chat_memory=FileChatMessageHistory("messages.json"),
+#   memory_key="messages",
+#   return_messages=True,
+# )
 prompt = ChatPromptTemplate(
   input_variables=["content", "messages"],
   messages=[
@@ -30,6 +41,7 @@ chain = LLMChain(
   llm=chat,
   prompt=prompt,
   memory=memory,
+  verbose=True,
 )
 
 while True:
