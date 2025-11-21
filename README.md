0ed5071b4a58cae7b828f28997c924e69c8bfa94
Jack Toumey <jack@toumey.io>
2025-11-21 13:46:00 -0700

    chat working with context

---
+++ README.md
@@ -0,0 +1,5 @@
+## Run
+
+`uv run --env-file .env -- python main.py`
+
+Make sure to add `.env` file with `OPENAI_API_KEY`.
--- main.py
+++ main.py
@@ -1,6 +1,47 @@
-def main():
-    print("Hello from tchat!")
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+
+# from dotenv import load_dotenv
+from langchain.prompts import (
+  MessagesPlaceholder,
+  HumanMessagePromptTemplate,
+  ChatMessagePromptTemplate,
+  ChatPromptTemplate,
+)
+
+# load_dotenv()
+
+chat = ChatOpenAI()
+
+memory = ConversationBufferMemory(
+  memory_key="messages",
+  return_messages=True,
+)
+
+prompt = ChatPromptTemplate(
+  input_variables=["content", "messages"],
+  messages=[
+    MessagesPlaceholder(variable_name="messages"),
+    HumanMessagePromptTemplate.from_template("{content}"),
+  ],
+)
+
+chain = LLMChain(
+  llm=chat,
+  prompt=prompt,
+  memory=memory,
+)
+
+while True:
+  content = input(">> ")
+  result = chain({"content": content})
+  print(result["text"])
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#   print("Hello from tchat!")
+
+
+# if __name__ == "__main__":
+#   main()
